df74958810d78b1359a7f57867debb8917ff3370
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2024-04-30 15:03:17 -0600

    LakeShore 335 tested working

---
+++ examples/TempController_Test.py
@@ -0,0 +1,5 @@
+from heimdallr.all import *
+
+log = LogPile()
+
+tc = LakeShoreModel335("GPIB::12::INSTR", log)
--- src/heimdallr/instrument_control/drivers/LakeShore_Model335_dvr.py
+++ src/heimdallr/instrument_control/drivers/LakeShore_Model335_dvr.py
@@ -4,17 +4,22 @@
 from heimdallr.instrument_control.categories.all_ctgs import *
 
 class LakeShoreModel335(PIDTemperatureControllerCtg):
-
+	
+	RANGE_OFF = 0
+	RANGE_LOW = 1
+	RANGE_MID = 2
+	RANGE_HIGH = 3
+	
 	def __init__(self, address:str, log:LogPile):
 		super().__init__(address, log)
 	
 	def set_setpoint(self, temp_K:float, channel:int=1):
 		self.inst.write(f"SETP {channel},{temp_K}")
-	def get_setpoint(self, temp_K:float, channel:int=1):
-		self.inst.write(f"SETP? {channel}")
+	def get_setpoint(self, channel:int=1):
+		return float(self.inst.query(f"SETP? {channel}"))
 	
 	def get_temp(self, temp_K:float, channel:int=1):
-		self.inst.query(f"SETP? {channel}")
+		return float(self.inst.query(f"SETP? {channel}"))
 	
 	def set_pid(self, P:float, I:float, D:float, channel:int=1):
 		P = max(0.1, min(P, 1000))
@@ -30,12 +35,27 @@ class LakeShoreModel335(PIDTemperatureControllerCtg):
 	def get_pid(self, channel:int=1):
 		
 		pid_str = self.query(f"PID? {channel}")
-		#TODO: Process string
-		return None
+		print(pid_str)
+		
+		# Split at commas
+		pidstr_list = pid_str.split(",")
+		
+		# Format into array of numbers
+		pid_list = [float(x) for x in pidstr_list]
+		
+		# Return values as dictionary
+		return {"P": pid_list[0], "I": pid_list[1], "D": pid_list[2]}
 	
-	def set_range():
-		pass
-		#Will be RANGE command
+	def set_range(self, range:int, channel:int=1):
+		
+		range = max(0, min(range, 3))
+		if range != range:
+			self.log.error(f"Did not apply command. Invalid range parameter supplied. Use range constants built into class.")
+			return
+		
+		self.write(f"RANGE {channel},{range}")
+	def get_range(self, channel:int=1):
+		return int(self.query(f"RANGE? {channel}"))
 	
 	def set_enable(self, enable:bool, channel:int=1):
 		pass
