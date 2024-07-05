41527c94cc0eaae2bd55c6365f1573e5871d79b1
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2024-07-05 08:50:34 -0600

    Implementing pyfrost

--- src/heimdallr/networking/network.py
+++ src/heimdallr/networking/network.py
@@ -1,4 +1,23 @@
 
+from pyfrost.pyfrost import *
+from pyfrost.pf_client import *
+
+# def client_handler(ca:ClientAgent, opt:ClientOptions, words:list) -> bool:
+# 	''' Handles commands on the client side'''
+	
+# 	# This function would only be needed if commandline_main() were going to be called, rather than just direct function calls.
+	
+# 	return False
+
+# class HeimdallrClientAgent(ClientAgent):
+# 	''' Custom version of ClientAgent with functions specialized for Heimdallr.
+# 	'''
+	
+# 	def __init__(self, **kwargs):
+# 		super().__init__(**kwargs)
+	
+# 	def register
+
 class RemoteInstrument:
 	''' Class to represent an instrument driven by another host on this network. This
 	class allows remote clients to control the instrument, despite not having a 
@@ -10,19 +29,36 @@ class RemoteInstrument:
 		# Save values
 		self.remote_id = remote_id
 		self.remote_address = remote_address
-		self.client_agent = None
+		self.client_agent = ca
+		
+		self.connected = False # True if sucessfully connected to a remote instrument via server.
 		
 		# Register with server - this will populate
 		self.register_instrument(remote_id=self.remote_id, remote_address=self.remote_address)
 	
-	def register_instrument(remote_id:str=None, remote_address:str=None):
-		pass
-	
-		#TODO:  Ask server for instrument info
+	def register_instrument(self, remote_id:str=None, remote_address:str=None):
 		
-		#TODO: Update id and address
-		# self.remote_id = 
-		# self.remote_address = 
+		#TODO: Verify that remote_id or remote_address are valid
+		
+		# Tell server you wish to connect to this instrument
+		gc = GenCommand("REG-INST", {"REMOTE-ID":remote_id, "REMOTE-ADDR":remote_address })
+		
+		data_packet = self.client_agent.send_query(gc)
+		
+		# Check for missing packet
+		if data_packet is None:
+			self.connected = False
+			return
+		
+		# Check for error in packet
+		if not data_packet.validate_reply(['REMOTE_ID', 'REMOTE_ADDRESS'], self.log):
+			self.connected = False
+			return
+		
+		# Update data
+		self.remote_id = data_packet.data['REMOTE_ID']
+		self.remote_address = data_packet.data['REMOTE_ADDRESS']
+		self.connected = True
 	
 	def remote_call(self, func_name:str, *args, **kwargs):
 		''' Calls the function 'func_name' of a remote instrument '''
@@ -34,7 +70,9 @@ class RemoteInstrument:
 			arg_str = arg_str + f"{key}:{value} "
 		
 		print(f"Initializing remote call: function = {func_name}, arguments = {arg_str} ")
-
+		
+		#TODO: Send command to server
+	
 def RemoteFunction(func):
 	'''Decorator to allow empty functions to call
 	their remote counterparts'''
