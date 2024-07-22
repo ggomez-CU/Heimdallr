0c2c7311b968ea62b4ef241ec88a4859e7dd8064
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2024-07-22 16:51:20 -0600

    Created FSE driver, tested working

--- src/heimdallr/instrument_control/drivers/all_drivers.py
+++ src/heimdallr/instrument_control/drivers/all_drivers.py
@@ -4,6 +4,7 @@ from heimdallr.instrument_control.drivers.Keysight_8360L_dvr import *
 from heimdallr.instrument_control.drivers.LakeShore_Model335_dvr import *
 from heimdallr.instrument_control.drivers.Siglent_SSA3000X_dvr import *
 from heimdallr.instrument_control.drivers.Agilent_E4400_dvr import *
+from heimdallr.instrument_control.drivers.RohdeSchwarz_FSE_dvr import *
 from heimdallr.instrument_control.drivers.RohdeSchwarz_NRX_dvr import *
 from heimdallr.instrument_control.drivers.RohdeSchwarz_NRP_dvr import *
 from heimdallr.instrument_control.drivers.RohdeSchwarz_FSQ_dvr import *
