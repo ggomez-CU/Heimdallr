cc93428f81cded0098ef561d84a9f99bb30cb451
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2024-07-31 17:56:36 -0600

    Fixed typos

--- src/heimdallr/instrument_control/categories/digital_multimeter_ctg.py
+++ src/heimdallr/instrument_control/categories/digital_multimeter_ctg.py
@@ -10,8 +10,8 @@ class DigitalMultimeterCtg1(DigitalMultimeterCtg0):
 	# TODO: Flesh out
 	MEAS_RESISTANCE_2WIRE = "resistance-2wire"
 	MEAS_RESISTANCE_4WIRE = "resistance-4wire"
-	MEAS_VOLT_AC "voltage-ac"
-	MEAS_VOLT_DC "voltage-dc"
+	MEAS_VOLT_AC = "voltage-ac"
+	MEAS_VOLT_DC = "voltage-dc"
 	
 	# TODO: Flesh out
 	RANGE_AUTO = "auto-range"
